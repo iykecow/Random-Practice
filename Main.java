b6d9e6cca40421f72accbba958defe9fcd370adf
iykegoesmoo <iykecow@gmail.com>
2025-05-28 21:55:09 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,36 +1,11 @@
 public class Main {
   public static void main(String[] args) {
     
-    for (int outer = 1; outer <= 6; outer++) {
-      
+    int[][] matrix = new int[2][1];
 
-      for (int inner = outer; inner <= 6; inner++) {
-
-        if (inner % 2 == 0) {
-          System.out.print(inner + " ");
-        }
-
-      }
-
-      System.out.println();
-
-    }
-
-
-
-
-
-
-
-
-
-
-
+    System.out.println(matrix[0].length);
 
   }
-
-
-
 
 
 
